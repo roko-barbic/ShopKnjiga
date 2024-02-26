ce5f4d0099686a5f8e6c5f3959a581fa1f0e25b7
roko-barbic <rokobarosk12345@gmail.com>
2024-02-26 19:47:46 +0100

    Add Category, itd

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopKnjiga.Models;
+
+namespace ShopKnjiga.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+         
+    }
+
+    public DbSet<Category> Categories { get; set; }  
+}
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace ShopKnjiga.Models; //napisi ; i on ce sam maknit {}, dode ti na isto
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+
+
+}
