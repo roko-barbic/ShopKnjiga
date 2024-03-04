c3b3ec444baa3132d2de25e56afd0e51cba59ee3
roko-barbic <rokobarosk12345@gmail.com>
2024-03-04 19:43:29 +0100

    Add category controler and page

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopKnjiga.Data;
+using ShopKnjiga.Models;
+
+namespace ShopKnjiga.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,5 +10,28 @@ public class ApplicationDbContext : DbContext
          
     }
 
-    public DbSet<Category> Categories { get; set; }  
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new
+            {
+                Id = 1,
+                Name = "SciFi",
+                DisplayOrder = 1
+            },
+            new 
+            {
+                Id = 2,
+                Name = "Novel",
+                DisplayOrder = 2
+            },
+            new 
+            {
+                Id = 3,
+                Name = "Thriller",
+                DisplayOrder = 3
+            }
+            );
+    }
 }
